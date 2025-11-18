e051fc077d67fe24f4bf1e7c24e24fcd7868145b
Juan Sebastian Vargas <151252035+Jusebbb@users.noreply.github.com>
2025-11-18 17:03:19 -0500

    Update ClinicaRepository.java

---
+++ src/main/java/com/vargascortes/parcialsvargas/repository/ClinicaRepository.java
@@ -0,0 +1,7 @@
+package com.vargascortes.parcialsVargas.repository;
+
+import com.vargascortes.parcialsVargas.model.Clinica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClinicaRepository extends JpaRepository<Clinica, Long> {
+}
